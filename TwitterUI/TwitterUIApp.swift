89f9de099e2500a03a772638aacf9395fa27a938
kejlor <woj450@gmail.com>
2023-02-28 15:09:26 +0100

    Initial Commit

---
+++ TwitterUI/TwitterUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterUIApp.swift
+//  TwitterUI
+//
+//  Created by Bartosz Wojtkowiak on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
